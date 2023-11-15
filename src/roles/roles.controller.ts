54d7dc8aa2f40ca19a6ca487a2c95d125c2fb5dd
Dmutre <dmytro.lesko@gmail.com>
2023-11-15 19:46:17 +0100

    Update 0.0.2

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDTO } from './dto/CreateRoleDTO';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Roles')
 @Controller('roles')
 export class RolesController {
   constructor(private roleService: RolesService) {}
