477db184c53c84e7b9a458f5a9c9eddf171cdfe5
Dmutre <dmytro.lesko@gmail.com>
2023-11-15 17:24:49 +0100

    0.0.2
    Made connection between roles and users, now we can create user with default role "USER" .

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Role } from './roles.model';
+import { CreateRoleDTO } from './dto/CreateRoleDTO';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+
+  constructor(@InjectModel(Role) private roleRepository: typeof Role) {}
+
+  async createRole(dto: CreateRoleDTO) {
+    const role = await this.roleRepository.create(dto);
+    return role;
+  }
+
+  async getRoleByValue(value: string) {
+    const role = await this.roleRepository.findOne({where: {value}});
+    return role;
+  }
+}
