78959e259f7598f18b72baa1dbcd4df6ba0723db
Dmutre <dmytro.lesko@gmail.com>
2023-12-14 16:18:16 +0100

    0.0.5
    Now server can chare static

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { UserRoles } from "./roles/user-roles.model";
 import { AuthModule } from './auth/auth.module';
 import { Post } from "./posts/posts.model";
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from 'path';
 
 @Module({
   controllers: [],
@@ -16,6 +18,9 @@ import { FilesModule } from './files/files.module';
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static')
     }),
     SequelizeModule.forRoot({
     dialect: 'postgres',
